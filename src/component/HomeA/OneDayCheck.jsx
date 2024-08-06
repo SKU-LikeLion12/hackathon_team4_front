09b0811f193d13973a87d4d6b9ef14b1b9b170c2
ungheeyo <rkdwl999@naver.com>
2024-08-06 21:15:16 +0900

    fix: 지영변경사항

---
+++ src/component/HomeA/OneDayCheck.jsx
@@ -0,0 +1,98 @@
+import React, {useState, useEffect} from 'react';
+import OneDay from './OneDay'
+import axios from 'axios';
+
+function OneDayCheck() {
+  const [selectedButtons, setSelectedButtons] = useState({});
+  const [isSubmitEnabled, setIsSubmitEnabled] = useState(false); // 버튼 상태 값 판단
+
+
+  const handleButtonClick = (count,value) => {
+      setSelectedButtons((prev) => ({ ...prev, [count]:value}));
+  };
+
+  // 백으로 보내는 부분 // O : True, X: False 상태로 백으로 보냄? header에는 토큰넣기 
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post('http://localhost:8080/dailycheck/add', {selectedButtons});
+      console.log('전송완료!', response.data);
+      // 제출완료된 후 상태 초기화
+      setSelectedButtons({});
+    } catch (error) {
+      console.error('Error submitting data:', error);
+    }
+  };
+
+  useEffect(() => {
+    // 모든 질문에 응답이 있는지 CK
+    const allAnswered = ['1','2','3','4'].every((key)=> selectedButtons[key]);
+    setIsSubmitEnabled(allAnswered);
+  }, [selectedButtons]);
+
+
+  const dates = [
+    {
+    count : '1',
+    img : "img/sleep.png",
+    qs : '충분한 수면을 취했나요?',
+    },
+  
+    {
+      count : '2',
+      img : "img/sick.png",
+      qs : '특별하게 아픈 곳은 없었나요?',
+      },
+  
+    {
+      count : '3',
+      img : "img/exercise1.png",
+      qs : '활동적으로 움직였나요?',
+    },
+    {
+      count : '4',
+      img : "img/happy.png",
+      qs : ' 오늘 하루 행복한가요?',
+    },
+  
+  ];
+
+  // 렌더링 되는 부분 
+  return (
+    <div className="bg-[#f0f7ff] py-[60px] ">
+      <div className="text flex items-center justify-center">
+          <div className="text-5xl font-extrabold">
+              <p>사자님의 건강상태를</p> 
+              <br/>
+              <p>체크해보아요!</p>  {/*사용자이름 props로 받아야하나? */}
+          </div>
+          <div className="ml-0">
+            <img className="w-30 h-30" src="img\medicine.png" alt='약'></img>
+          </div>
+      </div>
+      {/* <div className="bg-[#f0f7ff] py-[60px]"> */}
+      <div className="OneDayCheckBox">
+            {dates.map((date) => (
+              <OneDay
+                  key={date.count}
+                  count={date.count}
+                  img={date.img}
+                  qs={date.qs}
+                  selectedButton={selectedButtons[date.count]}
+                  handleButtonClick={handleButtonClick}
+              />
+            ))}
+      </div>
+      <div className="grid justify-items-center pt-10">
+          <button className="Submit-btn bg-[#68aefe] text-white font-bold px-6 py-3 rounded-md shadow-md disabled:bg-[#dadada] disabled:text-white font-bold"
+          onClick={handleSubmit}
+            disabled={!isSubmitEnabled} 
+            >
+           {/*handleSubmit버튼 클릭시 selectedButtons데이터를 백으로 보냄*/}
+            체크완료
+          </button>
+      </div>
+    </div>
+  );
+};
+
+export default OneDayCheck;
---
+++ src/component/Medicine/DonutChart_del.jsx
@@ -0,0 +1,128 @@
+import React, {useState, useEffect} from "react";
+import {Doughnut} from "react-chartjs-2";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+import {
+	Chart as ChartJS,
+	ArcElement,
+	Tooltip,
+	Legend,
+	plugins,
+} from "chart.js"; // 이걸 적용 안하면 오류
+import axios from "axios";
+
+ChartJS.register(
+	ArcElement,
+	Tooltip,
+	Legend,
+	ChartDataLabels
+);
+
+export default function DonutChart_del() {
+	// const [percent, setPercent] = useState(0);
+	const percent = 100; // 임의 데이터였던 것
+
+	// useEffect(() => {
+	//   const MedicinePercent = async () => {
+	//     try {
+	//       const response = await axios.get('url'); // 백엔드 엔드포인트로 변경
+	//       setPercent(response.data.percent); // 백엔드에서 받은 percent 데이터로 설정
+	//     } catch (error) {
+	//       console.error('Error MedicenePercent: ', error);
+	//     }
+	//   };
+	//   MedicinePercent();
+	// }, []);
+
+	const data = {
+		labels: ["약 복용"], // 커서 올렸을 때 뜨는값
+		datasets: [
+			// 데이터 관련 정보(값, 컬러, 라벨 등)를 담은 dataset 객체들을 가지고 있는 배열
+			{
+				data: [percent, 100 - percent], // 약 섭취량, 총섭취량-실섭취량
+				backgroundColor: ["#9DCEFF", "#F2F3F6"],
+				borderWidth: [0, 0], // border적용
+				scaleBeginAtZero: true,
+				borderRadius: [20, 0], // 그래프 끝 radius 설정
+				// 도넛 안쪽 원의 크기 설정
+			},
+		],
+	};
+
+	const options = {
+		// 옵션 찾아보기
+		cutout: "70%",
+		responsive: true, // 알아서 동적으로
+		maintainAspectRatio: false,
+		animaition: {
+			animateScale: true,
+			animateRotate: true,
+		},
+		rotation: 0, // 회전해서 위치 조정가능
+		circumference: 360, // 크기? 같은거
+		plugins: {
+			legend: {
+				display: true, // 상단에 뜨는거
+				labels: {
+					filter: function (legendItem, data) {
+						//100-percent 부분 범례 표시x 필터링
+						return legendItem.index === 0;
+					},
+				},
+			},
+			tooltip: {
+				callbacks: {
+					label: function (tooltipItem) {
+						// 100-percent부분에 값이 표시되지 않도록 설정
+						if (tooltipItem.dataIndex === 1) {
+							return "";
+						}
+						return `${tooltipItem.label} : ${tooltipItem.raw}%`;
+					},
+				},
+			},
+			datalabels: {
+				display: function (context) {
+					return context.dataIndex === 0; // 첫번째 데이터만 라벨 표시
+				},
+				color: "white",
+				font: {
+					weight: "bold",
+				},
+				formatter: (value) => `${value}%`,
+			},
+			// formatter: (value, context) => {
+			//   if (context.dataIndex === 0) {
+			//     return `${value}%`;
+			//   } else {
+			//     return '';
+			//   }
+			// }
+		},
+	};
+
+	// 백한테 데이터 받아옴
+	// useEffect(() => {
+	//   const getData = async () => {
+	//     try {
+	//       const response = await axios.get('url');
+	//       setPercentage(response.data.percentage);
+	//     } catch (error) {
+	//       console.error('Error fetching percentage data: ', error);
+	//     }
+	//   };
+	//   getData();
+	// }, []);
+
+	// 차트에 넣을 데이터(객체를 넣기) (백이 percentage 를 넘겨줌)
+
+	// 렌더링값
+	return (
+		<div className='chartContainer w-[320px] h-[400px]'>
+			{" "}
+			{/* 여기서 차트 크기 조정  */}
+			<Doughnut data={data} options={options} />
+			{/* <Chart type ='doughnut' data={data}/> */}
+		</div>
+		// <div style ={{positon: 'center', width: '200px', height:'200px'}}>
+	);
+}
